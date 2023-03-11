25c8124e7db865e151b765045672295f02e3b2dd
Anil Jain <aniljain1982@gmail.com>
2023-03-11 16:13:27 +0100

    setup steps

--- src/test/java/com/automation/windows/notepad/NotePadTests.java
+++ src/test/java/com/automation/windows/notepad/NotePadTests.java
@@ -1,5 +1,8 @@
 package com.automation.windows.notepad;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -16,14 +19,28 @@ public class NotePadTests {
 		DriverManagerFactory factory = new DriverManagerFactory();
 		driver = factory.getDriverManager("Windows").getDriver();
 	}
-	
+
+	/*
+	 * 1. Open “Notepad” 2. Open “Help” menu item 3. Press “About Notepad” 4. Press
+	 * “OK” button
+	 */
 	@Test
 	public void test_1() {
 		driver.findElement(By.name("Help")).click();
 		driver.findElement(By.name("About Notepad")).click();
 		driver.findElement(By.name("OK")).click();
 	}
-	
+
+	/*
+	 * 1. Open “Notepad” 2. Type current date 3. Clear the results
+	 */
+	@Test
+	public void test_2() {
+		driver.findElement(By.className("Edit")).sendKeys(LocalDateTime.now().toString());
+		driver.findElement(By.className("Edit")).clear();
+	}
+
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
